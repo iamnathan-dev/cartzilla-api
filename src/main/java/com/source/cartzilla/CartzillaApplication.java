66739275e8591fcc09bab8b8f8adc355f5f83b3f
iamnathan-dev <nathancodes05@gmail.com>
2026-01-02 14:05:57 +0100

    project init

---
+++ src/main/java/com/source/cartzilla/CartzillaApplication.java
@@ -0,0 +1,13 @@
+package com.source.cartzilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartzillaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartzillaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/source/cartzilla/CartzillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.source.cartzilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartzillaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
